ce89cb86db0c5f190f1054298d58c31ffa7c107d
nicolocamassa <camassanicolo03@gmail.com>
2024-03-01 18:58:15 +0100

    [BONUS] pagination added

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -12,23 +12,46 @@ export default {
     data(){
         return{
             store,
-        }
+            page: 1,
+            totalPages: null
+        };
     }, mounted(){
-        axios.get(`${this.store.apiPath}/api/projects`).then((response) => {
-            this.store.projects = response.data.result;
-        });
+        this.apiRequest();
+    }, methods: {
+        apiRequest(){
+            axios.get(`${this.store.apiPath}/api/projects?page=${this.page}`).then((response) => {
+                this.store.projects = response.data.result.data;
+                this.totalPages = response.data.result.last_page;
+            });
+        },
+
+        nextPage(){
+            if (this.page < this.totalPages) { 
+                this.page++;
+                this.apiRequest();
+            }
+        },
+
+        prevPage(){
+            if (this.page != 1) { 
+                this.page--;
+                this.apiRequest();
+            }
+        }
     }              
 }
 </script>
 <template lang="">
     <main class="p-5">
 
-        <div class="container">
+        <div class="container mx-auto">
             <div class="row">
                     <Card v-for="project, index in this.store.projects" :project="project" :key="index" />
             </div>
         </div>
-
+        
+        <button type="button" class="btn btn-primary me-2" @click="prevPage">Previous</button>
+        <button type="button" class="btn btn-primary" @click="nextPage">Next</button>
     </main>
 </template>
 <style lang="">
