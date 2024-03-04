396fb50c393ea46c696bf85cc5c875dcb5ed0bc3
nicolocamassa <camassanicolo03@gmail.com>
2024-03-04 15:39:10 +0100

    [ESERCIZIO FINITO] added link to router

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,28 +1,35 @@
 <script>
+import { store } from "../store";
+
 export default {
-    name: 'Header',    
-}
+  name: "Header",
+  data() {
+    return {
+      store, // This makes the store available in the template
+    };
+  },
+};
 </script>
-<template lang="">
-    <header>
-        <div class="logo fs-1 fw-bolder">Logo</div>
-        <div class="menu">
-            <ul>
-                <li>
-                    
-                </li>
-            </ul>
-        </div>
-    </header>
+<template>
+  <header>
+    <div class="logo fs-1 fw-bolder">Logo</div>
+    <div class="menu">
+      <ul class="d-flex list-unstyled">
+        <li class="me-4" v-for="(item, index) in store.menu" :key="index">
+          <router-link class="text-dark text-decoration-none fw-bold" :to="{ name: item.name }">{{ item.label }}</router-link>
+        </li>
+      </ul>
+    </div>
+  </header>
 </template>
 <style lang="scss">
-    header{
-        width: 100%;
-        height: 100px;
-        background-color: rgb(206, 206, 206);
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        padding: 0 200px;
-    }
+header {
+  width: 100%;
+  height: 100px;
+  background-color: rgb(206, 206, 206);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 200px;
+}
 </style>
--- src/store.js
+++ src/store.js
@@ -3,11 +3,11 @@ import { reactive } from "vue";
 export const store = reactive({
     apiPath: 'http://127.0.0.1:8000',
     projects: [],
-    Menu: [
+    menu: [
         {
             name: 'home',
             label: 'Home'
-        }
+        },
         {
             name: 'blog',
             label: 'Blog'
