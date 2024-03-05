3f138b34bfd6accb9b353cf980d3373bafe8146c
nicolocamassa <camassanicolo03@gmail.com>
2024-03-05 16:49:06 +0100

    [ESERCIZIO FINITO] view all data in view page

--- src/pages/AppView.vue
+++ src/pages/AppView.vue
@@ -1,13 +1,34 @@
-<template lang="">
-    <div>
-        test
-    </div>
-</template>
 <script>
+import { store } from '../store.js';
+import axios from 'axios';
+
 export default {
-    
-}
+    data() {
+        return {
+            store,
+            project: null
+        }
+    }, 
+    created(){
+        this.getCardInfo();
+    },
+    methods: {
+        getCardInfo(){
+            axios.get(`${this.store.apiPath}/api/projects/${this.$route.params.slug}`).then((response) => {
+                this.project = response.data.project;
+            });
+        }
+    }    
+};
 </script>
-<style lang="">
-    
-</style>
+<template lang="">
+   <div v-if="project" class="text-dark">
+        <img :src="`${this.store.apiPath}/storage/${project.image}`" alt="">
+        <h1>{{ project.title }}</h1>
+        <p>{{ project.description }}</p>
+        <p>{{ project.date }}</p>
+        <p>{{ project.languages }}</p>
+    </div>
+    <div v-else>Loading...</div>
+</template>
+<style lang=""></style>
